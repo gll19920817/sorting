a61d60878f5296a61563110e5fd1d8f004ba49c4
耿伦伦 <gll19920817@gmail.com>
2022-03-16 22:07:28 +0800

    merge sort

---
+++ mergesort.php
@@ -0,0 +1,53 @@
+<?php
+function MS(&$arr) {
+    $len = sizeof($arr);
+    if($len < 2) {
+        return;
+    }
+
+    $mid = intval($len / 2);
+    for($i=0; $i<$mid; $i++) {
+        $L[] = $arr[$i];
+    }
+
+    for($i=$mid; $i<$len; $i++) {
+        $R[] = $arr[$i];
+    }
+
+    MS($L);
+    MS($R);
+
+    merge($L, $R, $arr);
+}
+
+function merge($arr1, $arr2, &$arr) {
+    $i = 0;
+    $j = 0;
+    $k = 0;
+    $len1 = sizeof($arr1);
+    $len2 = sizeof($arr2);
+
+    
+    while($i<$len1 && $j<$len2) {
+        if($arr1[$i] < $arr2[$j]) {
+            $arr[$k++] = $arr1[$i++];
+        } else {
+            $arr[$k++] = $arr2[$j++];
+        }
+    }
+
+    while($i<$len1) {
+        $arr[$k++] = $arr1[$i++];
+    }
+    
+    while($j<$len2) {
+        $arr[$k++] = $arr2[$j++];
+    }
+}
+
+
+$arr = [6,3,7,8,2,1,4,5];
+
+MS($arr);
+
+print_r($arr);
